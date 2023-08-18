0736cf254a4dec6f760dfd02a212d34b79282558
PaulChernin <pavel@chernin.ru>
2023-08-18 15:22:52 +0300

    use port from .env

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
 import { jwtMiddleware } from './src/jwtMiddleware.js'
 
-const port = 4000
+const port = process.env.PORT
 const app = express()
 app.use(cookieParser())
 app.use(cors({
@@ -33,5 +33,5 @@ app.use('/protected*', jwtMiddleware)
 app.use(router)
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`)
+    console.log(`Server is running at port ${port}`)
 })
